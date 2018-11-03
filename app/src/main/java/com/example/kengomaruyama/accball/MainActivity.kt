6d727aa43ef6513e616e5a54c2617ebec21097e2
klcatha <klcatha0518@gmail.com>
2018-11-03 14:34:21 +0900

    sensor event listener

--- app/src/main/java/com/example/kengomaruyama/accball/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/accball/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.kengomaruyama.accball
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
