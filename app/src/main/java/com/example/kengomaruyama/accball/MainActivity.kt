36bd3e05ac5aa30de013b649e74ec7523f18f961
klcatha <klcatha0518@gmail.com>
2018-11-03 14:58:08 +0900

    sensor changed method

--- app/src/main/java/com/example/kengomaruyama/accball/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/accball/MainActivity.kt
@@ -7,12 +7,20 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER){
+            Log.d("MainActivity",
+                    "x = ${event.values[0].toString()}" +
+                            ",y = ${event.values[1].toString()}" +
+                            ",z = ${event.values[2].toString()}")
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
